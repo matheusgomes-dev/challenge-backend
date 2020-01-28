a199960d7bacd763c75cb90a5304623f5959a54a
Matheus Gomes <mgomes@trinapse.com.br>
2020-01-28 20:49:06 -0300

    Correção realizada para exibir documentação do swagger da API --

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const app = require("./app");
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const routes = require("./routes");
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
@@ -9,14 +10,17 @@ require("dotenv").config({
   path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
 });
 
+const app = express();
+
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use(routes);
+app.use(cors());
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(`Server started on port ${process.env.PORT}`);
